a1b51af75a7cbc1f8d0eb08aeb64e836c241d0ee
pshah <contactshahpriyank@gmail.com>
2019-02-19 19:02:42 -0800

    First commit

---
+++ src/App.java
@@ -0,0 +1,38 @@
+import com.mpatric.mp3agic.Mp3File;
+import java.awt.BorderLayout;
+import java.awt.FlowLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.JTable;
+import javax.swing.table.TableColumn;
+
+//import StreamPlayerNew.ButtonListener;
+import javazoom.jlgui.basicplayer.BasicPlayer;
+import javazoom.jlgui.basicplayer.BasicPlayerException;
+
+public class App {
+	
+	//Mp3File mp3file = new Mp3File();
+	
+	/*
+	System.out.printlin("Length of this mp3 is: " + mp3file() + " seconds");
+	System.out.println("Bitrate: " + mp3file.getBitrate() + " kbps " + (mp3file.isVbr() ? "(VBR)" : "(CBR)"));
+	System.out.println("Sample rate: " + mp3file.getSampleRate() + " Hz");
+	System.out.println("Has ID3v1 tag?: " + (mp3file.hasId3v1Tag() ? "YES" : "NO"));
+	System.out.println("Has ID3v2 tag?: " + (mp3file.hasId3v2Tag() ? "YES" : "NO"));
+	System.out.println("Has custom tag?: " + (mp3file.hasCustomTag() ? "YES" : "NO"));
+	*/
+
+}
